0c4ac79c52495883bd9edbe06133cdc84d8d40ab
Felipe Alves <felipe.alvcor@gmail.com>
2020-12-07 21:05:33 -0300

    Insercao: O protocolo ja aceita uma nova conexao
    Com esta implementacao ja eh possivel passar no teste1 da pasta de
    testes.
    
    Foi necessario alterar a funcao de envio da classe Conexao, fora isso
    tambem foi necessario criar algumas novas variaveis para a classe
    conexao somente para ficar mais facil a compreensao desta mesma classe;

--- tcp.py
+++ tcp.py
@@ -1,24 +1,25 @@
 import asyncio
 from tcputils import *
-
+import random
 
 class Servidor:
     def __init__(self, rede, porta):
-        self.rede = rede
+        self.rede = rede                            #camada de rede que eh criada no teste
         self.porta = porta
         self.conexoes = {}
         self.callback = None
-        self.rede.registrar_recebedor(self._rdt_rcv)
+        self.rede.registrar_recebedor(self._rdt_rcv)#registrando o recebedor da camada de rede, ele esta definindo que a variavel callback da camada de rede vai chamar a funcao _rdt_rcv do servidor 
 
     def registrar_monitor_de_conexoes_aceitas(self, callback):
         """
         Usado pela camada de aplicação para registrar uma função para ser chamada
         sempre que uma nova conexão for aceita
         """
-        self.callback = callback
+
+        self.callback = callback #chamando a funcao conecao_aceita
 
     #Tudo que vier do protocolo IP vai passar por aqui
-    def _rdt_rcv(self, src_addr, dst_addr, segment):
+    def _rdt_rcv(self, src_addr, dst_addr, segment): #segment = cabecalho tcp
         src_port, dst_port, seq_no, ack_no, \
             flags, window_size, checksum, urg_ptr = read_header(segment)
 
@@ -30,17 +31,26 @@ class Servidor:
             return
 
         payload = segment[4*(flags>>12):] #removendo o cabecalho e obtendo somente o payload
-        
+
         id_conexao = (src_addr, src_port, dst_addr, dst_port)
 
         if (flags & FLAGS_SYN) == FLAGS_SYN:
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
+
+            seq_no=seq_no+1             # mandando o set_no ja somado com 1 devido estar abrindo uma nova conexao
+            ack_no=seq_no               #
+            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no)
+
             # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
             # fazer aqui mesmo ou dentro da classe Conexao.
             if self.callback:
                 self.callback(conexao)
+
+                flags=FLAGS_SYN|FLAGS_ACK   #flags necessarias para quando uma nova conexao eh iniciada
+
+                conexao.enviar(flags,payload)
+
         elif id_conexao in self.conexoes:
             # Passa para a conexão adequada se ela já estiver estabelecida
             self.conexoes[id_conexao]._rdt_rcv(seq_no, ack_no, flags, payload)
@@ -50,12 +60,21 @@ class Servidor:
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    def __init__(self, servidor, id_conexao,ack_no):
         self.servidor = servidor
         self.id_conexao = id_conexao
+
+        self.dst_addr = id_conexao[0]           #idconexao[0] eh o endereco que enviou o dado
+        self.dst_port = id_conexao[1]
+        self.src_addr = id_conexao[2]           #endereco do servidor
+        self.src_port = id_conexao[3]           #endereco do servidor
+
         self.callback = None
         self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
+
+        self.ack_no= ack_no
+        self.seq_no= random.randint(0, 0xffff)  #o seq no deve ser um numero aleatorio
 
     def _exemplo_timer(self):
         # Esta função é só um exemplo e pode ser removida
@@ -76,13 +95,17 @@ class Conexao:
         """
         self.callback = callback
 
-    def enviar(self, dados):
+    def enviar(self, flags, payload):
         """
         Usado pela camada de aplicação para enviar dados
         """
-        # TODO: implemente aqui o envio de dados.
-        # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
-        # que você construir para a camada de rede.
+
+        #make header: src_port, dst_port, seq_no, ack_no, flags
+        header=make_header(self.src_port, self.dst_port, self.seq_no, self.ack_no, flags)
+
+        segmento=fix_checksum(header, self.src_addr, self.dst_addr )
+        self.servidor.rede.enviar(segmento, self.dst_addr)                                  #enviando um dado de volta para o destinatario desta conexao
+
         pass
 
     def fechar(self):
