1cf71a4d0a357294e245f91a1a72f11a620b45a7
RicardoSzram <ricardo.szram.filho@gmail.com>
2020-12-13 18:41:39 -0300

    Test 6 implementação parcial

--- tcp.py
+++ tcp.py
@@ -1,6 +1,7 @@
 import asyncio
 from tcputils import *
 import random
+import time
 
 class Servidor:
     def __init__(self, rede, porta):
@@ -70,8 +71,26 @@ class Conexao:
         self.src_port = id_conexao[3]           
 
         self.callback = None
-        self.timer = 0  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
+        self.timer = 3  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
+        # estimatedRTT = 0.875 * estimatedRTT + 0.125*SampleRTT   // Com Alpha = 0.125
+        # devRTT = 0.75 * devRTT + 0.25 * abs(SampleRTT - estimatedRTT)  // Com Beta = 0.25
+        # timeoutInterval = estimatedRTT + 4*devRTT
+        self.timeEnvio = 0
+        self.timeConfirmacao = 0
+
+        self.ultimoSeq = -1
+        self.pacoteRuim = False
+
+        #self.primeiroEnvio = True
+
+        self.SampleRTT = 1
+        self.estimatedRTT = 1
+        self.devRTT = 0.5
+        self.timeoutInterval = 3
+        self.primeiraMensagem = True
+
+        
 
         self.ack_no= ack_no
         self.seq_no= random.randint(0, 0xffff)  #o seq no deve ser um numero aleatorio
@@ -91,11 +110,39 @@ class Conexao:
 
             #parando o timer independente da mensagem recebida
             self.timer.cancel()
+            # A flag mediu envio deveria verificar se foi feita alguma medição de mensagens de envio, pra poder alterar de forma 
+            # correta o SampleRTT, ademais, a flag primeiraMensagem mostra que não havia nenhuma medição de sampleRTT
+            # a flag primeiroEnvio eh pra ver se nao eh um segmento de retransmissao, mas acho que tem que mudar isso pra ver pelo seq e ack, 
+            # por exemplo pegando da fila
 
             #se possuir pacotes na fila de pacotes, entao o ultimo sera retirado pois a mensagem dele foi recebida
             if len(self.filaPacotes)>0:
-                self.filaPacotes.pop(0)
+                _,_, seq_no, _=self.filaPacotes[0]
+                if self.primeiraMensagem:
+                    self.ultimoSeq = -1
+                    self.primeiraMensagem = False
+                if self.ultimoSeq == seq_no and not self.pacoteRuim:
+                    self.timeConfirmacao = time.time()
+                    self.SampleRTT = self.timeConfirmacao - self.timeEnvio
+                    if self.primeiraMensagem or self.SampleRTT < 0.15:
+                        print("Primeira né meu")
+                        print(self.timeEnvio, self.timeConfirmacao)
+                        self.primeiraMensagem = False
+                        self.estimatedRTT = self.SampleRTT
+                        self.devRTT = self.SampleRTT/2
+                    else:
+                        print("Calculei essa caraca")
+                        self.devRTT = 0.75 * self.devRTT + 0.25 * abs(self.SampleRTT - self.estimatedRTT)
+                        self.estimatedRTT = 0.875 * self.estimatedRTT + 0.125 * self.SampleRTT 
+                    self.timeoutInterval = self.estimatedRTT + 4 * self.devRTT
+                    self.ultimoSeq = -1
+                    self.pacoteRuim = False
+                    print("SampleRTT: ", self.SampleRTT)
+                    print("estimatedRTT: ", self.estimatedRTT)
+                    print("devRTT: ", self.devRTT)
+                    print("timeoutInterval: ", self.timeoutInterval)
 
+                self.filaPacotes.pop(0)
                 #caso ainda tenha alguem na fila a funcao precisa ser chamada novamente para enviar o proximo pacote da fila
                 if len(self.filaPacotes)>0:
                     self.enviaPacote()
@@ -115,6 +162,8 @@ class Conexao:
                 self.callback(self, payload)                  # na camada de rede foi defenido como callback uma funcao que da um append no payload
                 self.ack_no += len(payload)
                 self.enviaConfirmacao(FLAGS_ACK)              #ACK de confirmacao nao possui payload, foi feita uma funcao pois devido ao passo5 so se envia algo para a funcao self.enviar ccaso queira aguardar o ack de resposta
+
+
                                 
     def registrar_recebedor(self, callback):
         """
@@ -134,7 +183,7 @@ class Conexao:
 
             self.filaPacotes.append([temporary_payload,flags, self.seq_no, False])
 
-            if len(self.filaPacotes)<=1:             #se a fila estava vazia entao o enviador eh chamado novamente
+            if len(self.filaPacotes)<=1:             #se a fila estava vazia entao o enviador eh chamado novamente 
                 self.enviaPacote()
 
             #Somando o seq_no para a proxima mensagem que for ser enviada ir corretamente
@@ -163,21 +212,34 @@ class Conexao:
 
         payload,flags, seq_no, jaPassou=self.filaPacotes[0]
 
+        if self.ultimoSeq != seq_no:
+            print("Charadinha")
+            self.timeEnvio = time.time()
+            self.ultimoSeq = seq_no
+            self.pacoteRuim = False
+        else:
+            self.pacoteRuim = True
+            print("Ric and Morta")
+
         header=make_header(self.src_port, self.dst_port, seq_no, self.ack_no, flags)   #make header: src_port, dst_port, seq_no, ack_no, flags
         
         segmento=fix_checksum(header + payload, self.src_addr, self.dst_addr )
         self.servidor.rede.enviar(segmento, self.dst_addr)
-
-        self.timer= asyncio.get_event_loop().call_later(1, self.enviaPacote)            #inicializando o timer do recebimento do ACK deste pacote
+        print("Esse é nosso timeout: ", self.timeoutInterval)
+        self.timer= asyncio.get_event_loop().call_later(self.timeoutInterval, self.enviaPacote)            #inicializando o timer do recebimento do ACK deste pacote
 
     def enviaConfirmacao(self, flags):
-
+        #self.timeEnvio = time.time()
         header=make_header(self.src_port, self.dst_port, self.seq_no, self.ack_no, flags)   #make header: src_port, dst_port, seq_no, ack_no, flags
         
         segmento=fix_checksum(header, self.src_addr, self.dst_addr )
         self.servidor.rede.enviar(segmento, self.dst_addr)
 
     def fechar(self):
+        if self.ultimoSeq != seq_no:
+            print("Fechou")
+            self.timeEnvio = time.time()
+            self.ultimoSeq = seq_no
         header=make_header(self.src_port, self.dst_port, self.seq_no, self.ack_no, FLAGS_FIN)   #make header: src_port, dst_port, seq_no, ack_no, flags
         
         segmento=fix_checksum(header, self.src_addr, self.dst_addr )
